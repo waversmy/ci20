7c2ca08f6b0794f4349c6d9affee89a99b06e4d7
Wavershan <31004598+Wavershan@users.noreply.github.com>
2020-02-29 18:06:15 -0500

    update

--- type/init.js
+++ type/init.js
@@ -3,6 +3,7 @@ var bgcolors = ["#CC543A", "#B481BB", "#3A8FB7", "#1560BD", "#6D2E5B", "#8B81C3"
 var letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 document.onkeydown = function (e) {
+  if (e.repeat) { return }
   console.log("key down", event);
   cursor.style.display = "none";
   if (e.keyCode == 65) { 
